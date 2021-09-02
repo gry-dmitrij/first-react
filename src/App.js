d87575686b8b33d33a6aef7a9d0f86c1d271e860
Dmitriy Gryaznykh <gry-dmitrij@yandex.ru>
2021-09-02 09:53:44 +0300

    - Задание ко второму уроку

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Message from "./components/Message";
 
 function App() {
-  return (
-    <div className="App">
-      <Message text="Первое приложение"></Message>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Message text="Первое приложение"/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,11 +1,67 @@
 import './style.scss'
+import {Fragment, useEffect, useRef, useState} from "react";
 
-function Message({ text }) {
+function Message() {
+    const botTimers = useRef([]);
+    const [messageList, setMessageList] = useState([]);
+    const [message, setMessage] = useState('');
+    const [author, setAuthor] = useState('Guest');
+
+    useEffect(()=> {
+        return () => {
+            // очищаем запущенные таймеры при размонтировании
+            botTimers.current.forEach(item => {
+                clearTimeout(item);
+            });
+        }
+    }, []);
+
+    const handleMessage = (event) => {
+        setMessage(event.target.value);
+    }
+
+    const handleAuthor = (event) => {
+        setAuthor(event.target.value);
+    }
+
+    // добавляем автора и сообщение в массив
+    const addMessage = (author, message) => {
+        setMessageList(prevList => [...prevList, {
+            message,
+            author: author.trim() ? author : 'Anonymous'
+        }]);
+    }
+
+    const formSubmit = (event) => {
+        event.preventDefault();
+        addMessage(author, message);
+        botMessage(author);
+    }
+
+    function botMessage (author) {
+        const timerId = setTimeout(() => {
+            botTimers.current.shift();
+            addMessage('Bot', `Сообщение от ${author} отправлено!`);
+        }, 1500);
+        botTimers.current.push(timerId);
+    }
+
     return (
-        <div>
-            <h1 className={'main-title'}>{text}</h1>
-            <p>Проверка работы <span>scss</span></p>
-        </div>
+        <Fragment>
+            <form action="#" onSubmit={formSubmit} className="chat-form">
+                <label htmlFor="author" className="label">Автор:</label>
+                <input type="text" id="author" className="edit" value={author} onChange={handleAuthor}/>
+                <label htmlFor="message">Сообщение:</label>
+                <textarea id="message" className="edit edit--area" value={message} onChange={handleMessage}/>
+                <input type="submit" className="chat-form__button" value="Отправить"/>
+            </form>
+            {messageList.map((item, index) =>
+                <Fragment key={index}>
+                    <div>Автор: {item.author}</div>
+                    <pre>Сообщение: {item.message}</pre>
+                </Fragment>
+            )}
+        </Fragment>
     );
 }
 
